fc0834c6d27bf27e5b252a4a4ec2e432baae83e2
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2024-04-09 14:20:24 +0530

    ✨ Getting products from server API on client

--- DAY3/ExpressBasics/static/script.js
+++ DAY3/ExpressBasics/static/script.js
@@ -1,1 +1,9 @@
-console.log("Script Loaded !");
+// write a method to fetch products (fetch API/axios)
+// console.log(products)
+async function GetProducts() {
+  let res = await fetch("http://localhost:3000/products");
+  let products = await res.json();
+  console.log(products);
+}
+
+document.addEventListener("DOMContentLoaded", GetProducts);
