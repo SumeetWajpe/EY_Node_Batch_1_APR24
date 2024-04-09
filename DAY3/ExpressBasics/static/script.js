7e50211ef32f45809bc36a92dd3742936b007c0f
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2024-04-09 14:57:18 +0530

    ✨ Display Products in UI

--- DAY3/ExpressBasics/static/script.js
+++ DAY3/ExpressBasics/static/script.js
@@ -3,7 +3,28 @@
 async function GetProducts() {
   let res = await fetch("http://localhost:3000/products");
   let products = await res.json();
-  console.log(products);
+  let productRow = document.querySelector(".row");
+  for (const product of products) {
+    productRow.innerHTML += DisplayProductCard(product);
+  }
+}
+
+function DisplayProductCard(product) {
+  let productCard = `
+  <div class="col-md-3 my-1">
+    <div class="card">
+  <img src=${product.imageUrl} width="200px" height="150px" class="card-img-top" alt=${product.title}/>
+  <div class="card-body">
+    <h5 class="card-title">${product.title}</h5>
+    <p class="card-text">₹.${product.price}</p>
+    <p class="card-text">${product.rating}</p>
+
+    <button  class="btn btn-primary">${product.likes}</button>
+  </div>
+</div>
+</div>
+    `;
+  return productCard;
 }
 
 document.addEventListener("DOMContentLoaded", GetProducts);
